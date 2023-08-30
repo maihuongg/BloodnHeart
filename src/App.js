e7dd0953e1b3e71d125ead2999e7c81c3ed4c958
maihuongg <dangmaihuong1108@gmail.com>
2023-08-30 15:50:37 +0700

    new template
    new template

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+// import './App.css'
+import React from 'react';
+import Header from './components/header';
+import Slider from './components/slider';
+import Home  from './components/home';
+import Footer from './components/footer';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Slider/>
+    <Home/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
