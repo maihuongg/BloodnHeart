b0baaf3b1bed8c47a2a6ee18830226c150ddef3e
maihuongg <dangmaihuong1108@gmail.com>
2023-08-30 12:35:17 +0700

    template
    template

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
