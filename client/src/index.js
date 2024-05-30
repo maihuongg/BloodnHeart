4782ae2bd0352a312b3c5bbfed94dc463d7e32b5
maihuongg <dangmaihuong1108@gmail.com>
2024-05-31 00:45:25 +0700

    config redux-persist
    fix redux-persist - state

--- client/src/index.js
+++ client/src/index.js
@@ -11,14 +11,17 @@ import { PersistGate } from "redux-persist/integration/react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-    <App />
-    </PersistGate>
-  </Provider>,
+persistor.purge().then(()=>{
+  root.render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      <App />
+      </PersistGate>
+    </Provider>,
+  
+  );
 
-);
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
