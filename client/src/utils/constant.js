171b02c8d131f9e011e56a804d7b0b28cdbb4b54
maihuongg <dangmaihuong1108@gmail.com>
2024-07-19 08:25:08 +0700

    reup

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,3 +1,3 @@
-// const baseUrl="https://bn-h-api.vercel.app"
-const baseUrl="http://localhost:8000"
+const baseUrl="https://bn-h-api.vercel.app"
+// const baseUrl="http://localhost:8000"
 export default baseUrl;
